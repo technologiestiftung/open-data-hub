7fca1c3969b40e4a1d01865db72162df65674014
fdnklg <fabian.dinklage@me.com>
2018-04-10 17:27:58 +0200

    style: navbar

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 import './styles/root.scss';
-console.log('parcel hot hot hot!')
+
+const test = () => {
+    console.log('input');
+};
+
+let color = false;
+
+const setColorNav = () => {
+    let scrollY = window.scrollY;
+    const navbar = document.querySelector('.navbar');
+
+    if (scrollY > 889 && scrollY < 1200 && color == false) {
+        navbar.classList.toggle('primary');
+        color = true;
+    } else if (scrollY > 6076 && color == true) {
+        navbar.classList.toggle('primary');
+        color = false;
+    } else if (scrollY < 890 && color == true) {
+        navbar.classList.toggle('primary');
+        color = false;
+    } else if (scrollY < 6077 && scrollY > 889 && color == false) {
+        navbar.classList.toggle('primary');
+        color = true;
+    }
+}
+
+document.addEventListener('scroll', () => { 
+    setColorNav();
+})
