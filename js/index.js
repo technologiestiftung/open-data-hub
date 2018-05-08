ce832f9d40abad0962f16576542a80780ee32388
fdnklg <fabian.dinklage@me.com>
2018-05-08 13:06:52 +0200

    nav fixed safari

--- js/index.js
+++ js/index.js
@@ -3,10 +3,10 @@
 var currentColor = false;
 
 var setColorNav = function setColorNav() {
-    var section_01 = document.getElementById('s_openData').getBoundingClientRect().y - 40;
-    var section_02 = document.getElementById('s_process').getBoundingClientRect().y - 40;
-    var section_03 = document.getElementById('s_schedule').getBoundingClientRect().y - 40;
-    var section_04 = document.getElementById('s_projects').getBoundingClientRect().y - 40;
+    var section_01 = document.getElementById('s_openData').getBoundingClientRect().top - 40;
+    var section_02 = document.getElementById('s_process').getBoundingClientRect().top - 40;
+    var section_03 = document.getElementById('s_schedule').getBoundingClientRect().top - 40;
+    var section_04 = document.getElementById('s_projects').getBoundingClientRect().top - 40;
     var scrollY = window.scrollY;
     var navbar = document.querySelector('.navbar');
 
